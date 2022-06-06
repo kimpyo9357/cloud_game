e8582ac303822c5f76cb837afe4d598b8eab4708
kimpyo9357 <kimpyo9357@naver.com>
2022-06-06 16:49:44 +0900

    make backend

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -112,6 +112,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+APPEND_SLASH = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -121,4 +122,5 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
