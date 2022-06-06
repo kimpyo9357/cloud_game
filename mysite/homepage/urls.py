1b89e1a6bde486aefb6c724bec4f51a2f3267d64
kimpyo9357 <kimpyo9357@naver.com>
2022-06-06 17:17:02 +0900

    fix information url

--- mysite/homepage/urls.py
+++ mysite/homepage/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('AuthorLogin/', views.authorlogin),
     path('Message/', views.message, name="message"),
     path('SignUp/', views.signup, name="signup"),
-    path('Information/(?P<pk>\d+)', views.information, name='information'),
+    path('Information', views.information, name='information'),
 
 ]
